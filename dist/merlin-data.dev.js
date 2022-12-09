6291318151d69deba49fa67a944289cb3c1e5b05
Gabriel Walker <gokuss4760@gmail.com>
2022-12-08 20:02:35 -0600

    finished merlin portion of game

--- dist/merlin-data.dev.js
+++ dist/merlin-data.dev.js
@@ -17,39 +17,35 @@ var data = [{
   attack: 15
 }]; // console.log(data["Sword slash"]);
 // console.log(data.trollHealth);
-// buttons = data;
 
 playerHealth.innerHTML = "Health: ".concat(data[1].merlinHealth);
 enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth);
-container.innerText = "What will will you do?"; // buttons.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         console.log(e.target.innerHTML);
-//     })
-// })
-
+container.innerText = "What will will you do?";
 attackButtons.forEach(function (button) {
   button.addEventListener("click", function () {
     if (button.innerHTML == "Fireball (Hot potato)") {
       enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth - data[1].Fireball);
       data[2].trollHealth -= data[1].Fireball;
-      container.innerText = "Merlin used Fireball (Hot potato)";
+      container.innerText = "Merlin used Fireball (Hot potato) \n";
     } else if (button.innerHTML == "Lightning Strike") {
       enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth - data[1].LightningStrike);
       data[2].trollHealth -= data[1].LightningStrike;
-      container.innerText = "Merlin used Lightning Strike";
+      container.innerText = "Merlin used Lightning Strike \n";
+    }
+
+    if (data[2].trollHealth > 0) {
+      // var returnAttack = Math.floor(Math.random() * 15) + 1;
+      playerHealth.innerHTML = "Health: ".concat(data[1].merlinHealth - data[2].attack);
+      data[1].merlinHealth -= data[2].attack;
+      container.innerHTML += " The Troll attacked you with ".concat(data[2].attack);
     } // console.log(data[1].Fireball)
     // console.log(data[2].trollHealth)
 
 
-    if (data[1].merlinHealth <= 0 || data[2].trollHealth <= 0) {
-      container.innerText = "The Troll is the dead!"; // container.innerText = (`YOU ARE DEAD`);
-    } else {
-      playerHealth.innerHTML = "Health: ".concat(data[1].merlinHealth - data[2].attack);
-      data[1].merlinHealth -= 5;
-      container.innerHTML += " The Troll attacked you with ".concat(data[2].attack);
-    } // if (data[1].merlinHealth <= 0) {
-    //     container.innerHTML = (`YOU ARE DEAD`);
-    // }
-
+    if (data[2].trollHealth <= 0) {
+      container.innerText = "The Troll is the dead!";
+    } else if (data[1].merlinHealth <= 0) {
+      container.innerText = "YOU ARE DEAD";
+    }
   });
 });
