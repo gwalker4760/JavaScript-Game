19af720a0ec66ddcf4d52aad81b048ad769945d1
Gabriel Walker <gokuss4760@gmail.com>
2022-12-12 09:00:36 -0600

    finished game

--- dist/knight-data.dev.js
+++ dist/knight-data.dev.js
@@ -6,28 +6,45 @@ var attackButtons = document.querySelectorAll(".attack");
 var container = document.querySelector(".eventFeed");
 var data = [{
   knightHealth: 40,
-  SwordSlash: 10,
+  SwordSlash: 20,
   ShieldBash: 10
 }, {
   merlinHealth: 20,
   Fireball: 20,
-  LightningStrike: 20
+  LightningStrike: 15
 }, {
   trollHealth: 40,
-  tackle: 5,
-  swing: 5
+  attack: 15
 }]; // console.log(data["Sword slash"]);
 // console.log(data.trollHealth);
-// buttons = data;
 
 playerHealth.innerHTML = "Health: ".concat(data[0].knightHealth);
 enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth);
-container.innerHTML = "Player has attacked!"; // buttons.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         console.log(e.target.innerHTML);
-//     })
-// })
-// buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//     })
-// })
+container.innerText = "What will will you do?";
+attackButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (button.innerHTML == "Sword slash") {
+      enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth - data[0].SwordSlash);
+      data[2].trollHealth -= data[0].SwordSlash;
+      container.innerText = "The Knight used Sword slash \n";
+    } else if (button.innerHTML == "Shield bash") {
+      enemyHealth.innerHTML = "Health: ".concat(data[2].trollHealth - data[0].ShieldBash);
+      data[2].trollHealth -= data[0].ShieldBash;
+      container.innerText = "The Knight used Shield bash \n";
+    }
+
+    if (data[2].trollHealth > 0) {
+      playerHealth.innerHTML = "Health: ".concat(data[0].knightHealth - data[2].attack);
+      data[0].knightHealth -= data[2].attack;
+      container.innerHTML += " The Troll attacked you with ".concat(data[2].attack);
+    } // console.log(data[1].Fireball)
+    // console.log(data[2].trollHealth)
+
+
+    if (data[2].trollHealth <= 0) {
+      container.innerText = "The Troll is the dead!";
+    } else if (data[0].knightHealth <= 0) {
+      container.innerText = "YOU ARE DEAD";
+    }
+  });
+});
--- knight-data.js
+++ knight-data.js
@@ -3,39 +3,55 @@ const enemyHealth = document.querySelector(".enemyHealth");
 const attackButtons = document.querySelectorAll(".attack");
 const container = document.querySelector(".eventFeed");
 
+
 const data = [
     {
     knightHealth: 40,
-    SwordSlash: 10,
+    SwordSlash: 20,
     ShieldBash: 10,
     },
     {
     merlinHealth: 20,
     Fireball: 20,
-    LightningStrike: 20,
+    LightningStrike: 15,
     },
     {
     trollHealth: 40,
-    tackle: 5,
-    swing: 5,
+    attack: 15,
     }]
 // console.log(data["Sword slash"]);
 // console.log(data.trollHealth);
 
-// buttons = data;
-
 playerHealth.innerHTML = (`Health: ${data[0].knightHealth}`);
 enemyHealth.innerHTML = (`Health: ${data[2].trollHealth}`);
-container.innerHTML = (`Player has attacked!`);
+container.innerText = (`What will will you do?`);
 
-// buttons.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         console.log(e.target.innerHTML);
-//     })
-// })
+attackButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.innerHTML == "Sword slash") {
+            enemyHealth.innerHTML = (`Health: ${data[2].trollHealth - data[0].SwordSlash}`);
+            data[2].trollHealth -= data[0].SwordSlash
+            container.innerText = (`The Knight used Sword slash \n`);
 
-// buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
+        } else if (button.innerHTML == "Shield bash") {
+            enemyHealth.innerHTML = (`Health: ${data[2].trollHealth - data[0].ShieldBash}`);
+            data[2].trollHealth -= data[0].ShieldBash
+            container.innerText = (`The Knight used Shield bash \n`);
+        }
 
-//     })
-// })
+        if(data[2].trollHealth > 0){
+            playerHealth.innerHTML = (`Health: ${data[0].knightHealth - data[2].attack}`);
+            data[0].knightHealth -= data[2].attack
+            container.innerHTML += (` The Troll attacked you with ${data[2].attack}`);
+        }
+        // console.log(data[1].Fireball)
+        // console.log(data[2].trollHealth)
+        
+        if  (data[2].trollHealth <= 0) {
+            container.innerText = (`The Troll is the dead!`);
+        } 
+        else if (data[0].knightHealth <= 0){
+            container.innerText = "YOU ARE DEAD";
+        }
+    })
+})
